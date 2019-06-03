fcdd6b4310e4b1533005d9bc5a9fdbfff0a7cd01
kmunge <orokomunge@gmail.com>
2019-06-03 16:54:11 +0300

    running tests

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,3 +1,4 @@
+
 from flask import render_template,redirect,url_for,flash,request
 from flask_login import login_user,logout_user,login_required
 from . import auth
--- config.py
+++ config.py
@@ -4,24 +4,25 @@ class Config:
     '''
     General configuration parent class
     '''
-    SECRET_KEY ='moo'
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOADED_PHOTOS_DEST ='app/static/photos'
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY ='moo'
+
 
     # simple mde  configurations
     SIMPLEMDE_JS_IIFE = True
     SIMPLEMDE_USE_CDN = True
     
 #  email configurations
-    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 
 class TestConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://omunge:123@localhost/pitches'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://omunge:123@localhost/blogs'
 
 class ProdConfig(Config):
     '''
@@ -38,11 +39,11 @@ class DevConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://omunge:123@localhost/pitches'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://omunge:123@localhost/blogs'
     DEBUG = True
 
 config_options = {
 'development':DevConfig,
-'production':ProdConfig,
+#'production':ProdConfig,
 'test':TestConfig
 }
